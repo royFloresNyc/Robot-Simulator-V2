9e9e729daa54e6711b57f1529182cf578efd528e
royFloresNyc <royfloresnyc@gmail.com>
2020-09-22 16:57:23 -0400

    Arrow key handler

--- index.js
+++ index.js
@@ -3,4 +3,33 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+  arrowHandler()
+  moveButtonHandler()
+  
 })
+
+const arrowHandler = () => {
+    document.addEventListener("keydown", function(e){
+        const newLi = document.createElement('li')
+
+        console.log(e.key)
+        if (e.key === "ArrowUp"){
+            newLi.textContent = "up"
+        } else if (e.key === "ArrowDown"){
+            newLi.textContent = "down"
+        } else if (e.key === "ArrowLeft"){
+            newLi.textContent = "left"
+        } else if (e.key === "ArrowRight"){
+            newLi.textContent = "right"
+        }
+
+        if (newLi.textContent) {
+            const parentUl = document.querySelector("#moves-container")
+            parentUl.append(newLi)
+        }
+    })
+}
+
+const moveButtonHandler = () => {
+    const moveButton = document.querySelector('button#move-button')
+}
